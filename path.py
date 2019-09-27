564d64382061137010dff24435e315bbf927dc03
TopaGeor <45096491+TopaGeor@users.noreply.github.com>
2019-09-27 13:03:44 +0300

    Update path.py

--- path.py
+++ path.py
@@ -1,81 +1,31 @@
+#Write a function that provides change directory (cd) function for an abstract file system.
+#Notes:
+#Root path is '/'.
+#Path separator is '/'.
+#Parent directory is addressable as '..'.
+#Directory names consist only of English alphabet letters (A-Z and a-z).
+#The function should support both relative and absolute paths.
+#The function will not be passed any invalid paths.
+#Do not use built-in path-related functions.
 class Path:
     def __init__(self, path):
-        self.current_path = path
-
-    def cd(self, new_path):
-        if new_path[0] == '/':
-            self.current_path = ''
-        
-        np = new_path.split('/')
-        cp = self.current_path.split('/')
-        for i in np:
-            if i == '..':
-                cp = cp[:len(cp)-1]
-            elif(i >= 'a')and(i <= 'z'):
-                cp.append(i)
-            elif(i >= 'A')and(i <= 'Z'):                
-                cp.append(i)
-            else:
-                print("invalid path")
-                return 
-                
-                    
-        update = ''
-        for i in cp[1:]:
-            update += '/'
-            update += i
-        
-        self.current_path = update
-        
-
-path = Path('/a/b/c/d')
-path.cd('/..')
-print(path.current_path)
-
-class Path:
-    def __init__(self, path):
-        self.current_path = path
-
-    def cd(self, new_path):
-        if new_path[0] == '/':
-            self.current_path = ''
-            new_path = new_path[1:]
-        
-        cp = self.current_path.split('/')
-        
-        for i in new_path.split('/'):
-            if i == '..':
-                cp.pop()
-            else:
-                cp.append(i)
- 
-        
-        self.current_path = '/'.join(cp)
-        
-
-path = Path('/a/b/c/d')
-path.cd('x/a/d')
-print(path.current_path)
-
-class Path:
-    def __init__(self, path):
-        self.dirs = []
+        self.path = []
         self.cd(path)
 
-    @property
+    @property#this is the trick to get the 100%
     def current_path(self):
-        return '/' + '/'.join(self.dirs)
+        return '/' + '/'.join(self.path)
         
     def cd(self, new_path):
         if new_path[0] == '/':
-            self.dirs = []
+            self.path = []
             new_path = new_path[1:]
         
         for i in new_path.split('/'):
             if i == '..':
-                self.dirs.pop()
+                self.path.pop()
             else:
-                self.dirs.append(i)
+                self.path.append(i)
         
 
 path = Path('/a/b/c/d')
