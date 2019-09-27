2a37c19d248ef7b99dfad65d2cf6462ba99c99d6
TopaGeor <45096491+TopaGeor@users.noreply.github.com>
2019-09-27 12:52:35 +0300

    Update League_Table.py

--- League_Table.py
+++ League_Table.py
@@ -1,4 +1,10 @@
 #10. League Table
+#The LeagueTable class tracks the score of each player in a league. After each game, the player records their score with the 
+#record_result function. 
+#The player's rank in the league is calculated using the following logic:
+#1) The player with the highest score is ranked first (rank 1). The player with the lowest score is ranked last.
+#2) If two players are tied on score, then the player who has played the fewest games is ranked higher.
+#3) If two players are tied on score and number of games played, then the player who was first in the list of players is ranked higher.
 
 from collections import Counter
 from collections import OrderedDict
