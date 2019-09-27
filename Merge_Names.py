97b637263c5baf5ca2437d32fbbc27e5a803aed0
TopaGeor <45096491+TopaGeor@users.noreply.github.com>
2019-09-27 12:53:49 +0300

    Update Merge_Names.py

--- Merge_Names.py
+++ Merge_Names.py
@@ -1,4 +1,9 @@
 #3. Merge Names
+#Implement the unique_names method. When passed two lists of names, it will return a list containing the names that appear in either or 
+#both lists. The returned list should have no duplicates.
+#For example, calling unique_names(['Ava', 'Emma', 'Olivia'], ['Olivia', 'Sophia', 'Emma']) should return a list containing Ava, Emma, 
+#Olivia, and Sophia in any order.
+
 def unique_names(names1, names2):
     ls = []
     for i in names1:
