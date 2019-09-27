aa42eaf273876421ebe117d5693c6e7b62aa51f5
TopaGeor <45096491+TopaGeor@users.noreply.github.com>
2019-09-27 12:56:15 +0300

    Update Quadratic_Equation.py

--- Quadratic_Equation.py
+++ Quadratic_Equation.py
@@ -1,4 +1,6 @@
 #5. Quadratic Equation
+#Implement the function find_roots to find the roots of the quadratic equation: ax2 + bx + c = 0. 
+#The function should return a tuple containing roots in any order.
 def find_roots(a, b, c):
     riza = b**2 - 4*a*c
     riza = riza**(0.5)
