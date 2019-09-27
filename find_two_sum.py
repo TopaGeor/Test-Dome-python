f4bec6330880f80f33bddce19cda49652b93de00
TopaGeor <45096491+TopaGeor@users.noreply.github.com>
2019-09-27 13:00:26 +0300

    Update find_two_sum.py

--- find_two_sum.py
+++ find_two_sum.py
@@ -1,3 +1,10 @@
+#8. Two Sum
+#Write a function that, when passed a list and a target sum, returns, efficiently with respect to time used, two distinct zero-based indices of any two of the numbers, whose sum is equal to the target sum. If there are no two numbers, the function should return None.
+#For example, find_two_sum([3, 1, 5, 7, 5, 9], 10) should return a single tuple containing any of the following pairs of indices:
+#0 and 3 (or 3 and 0) as 3 + 7 = 10
+#1 and 5 (or 5 and 1) as 1 + 9 = 10
+#2 and 4 (or 4 and 2) as 5 + 5 = 10
+
 def find_two_sum(numbers, target_sum):
     dict = {}  
     for i in range(0, len(numbers)):
@@ -6,7 +13,7 @@ def find_two_sum(numbers, target_sum):
         else:
             dict[numbers[i]].append(i)
     
-    numbers.sort() 
+    numbers.sort() #this is the trick to get the 100%
     for i in numbers:
         res = target_sum - i
         if(res in dict):
