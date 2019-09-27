428ace765fd7d11121ab9a16cf7c954b7fd3a9fb
TopaGeor <45096491+TopaGeor@users.noreply.github.com>
2019-09-27 12:57:43 +0300

    Update Song.py

--- Song.py
+++ Song.py
@@ -1,4 +1,9 @@
 #Song
+#A playlist is considered a repeating playlist if any of the songs contain a reference to a previous song in the playlist. Otherwise, 
+#the playlist will end with the last song which points to None.
+#Implement a function is_repeating_playlist that, efficiently with respect to time used, 
+#returns true if a playlist is repeating or false if it is not.
+#For example, the following code prints "True" as both songs point to each other.
 class Song:
     def __init__(self, name):
         self.name = name
