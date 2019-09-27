356ac0655bb77dfa82bbc1172948cade242ebf44
TopaGeor <45096491+TopaGeor@users.noreply.github.com>
2019-09-27 12:59:08 +0300

    Update file_owners.py

--- file_owners.py
+++ file_owners.py
@@ -1,4 +1,9 @@
 #1. File Owners
+#Implement a group_by_owners function that:
+#Accepts a dictionary containing the file owner name for each file name.
+#Returns a dictionary containing a list of file names for each owner name, in any order.
+#For example, for dictionary {'Input.txt': 'Randy', 'Code.py': 'Stan', 'Output.txt': 'Randy'} the group_by_owners
+#function should return {'Randy': ['Input.txt', 'Output.txt'], 'Stan': ['Code.py']}.
 def group_by_owners(files):
     dict = {}
     for i in files:
